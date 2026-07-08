07e175ee891c86c3aa3bb2dfc9d6433c11fcf0ea
tshis <tshisdaniel@gmail.com>
2026-07-08 12:50:37 +0200

    public lines booking system design done

--- src/Controller/web/public/LinesController.php
+++ src/Controller/web/public/LinesController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller\web\public;
 
+use App\Service\BusLayoutGridBuilder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -25,10 +27,30 @@ final class LinesController extends AbstractController
     } //results
 
     #[Route('/public/lines/{slug}/booking', name: 'public_lines_booking')]
-    public function booking(): Response
+    public function booking($slug, Request $request, BusLayoutGridBuilder $busLayoutGridBuilder): Response
     {
+        $session = $request->getSession();
+
+        $layouts = $session->get('bus_layout', []);
+
+        // simulation bus -> layout
+        $busToLayoutMap = [
+            'kin-matadi-trans-david-01' => 1,
+            'kin-matadi-trans-david-02' => 2,
+            'kin-matadi-trans-david-03' => 3,
+        ];
+
+        $layoutId = $busToLayoutMap[$slug] ?? null;
+
+        $busLayout = $layouts[$layoutId] ?? null;
+
+        if (!$busLayout) {
+            throw $this->createNotFoundException('Bus layout introuvable');
+        }
+
         return $this->render('public/lines/booking.html.twig', [
             'page' => 'lines',
+            'seatmap' => $busLayoutGridBuilder->build($busLayout),
         ]);
     } //booking
 
