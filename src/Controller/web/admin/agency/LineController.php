91e3525efcaa65b840c6055874a8a1261bd23ddd
tshis <tshisdaniel@gmail.com>
2026-07-09 17:55:29 +0200

    lines add design in progress

--- src/Controller/web/admin/agency/LineController.php
+++ src/Controller/web/admin/agency/LineController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\web\admin\agency;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -20,14 +21,27 @@ class LineController extends AbstractController
     #[Route('/admin/agency/add', name: 'admin_agency_line_add')]
     public function add(Request $request): Response
     {
+        // 1. Création du formulaire à la volée (sans entité)
+        $form = $this->createFormBuilder()
+            ->add('paysOrigine', CountryType::class, [
+                'placeholder' => 'Rechercher un pays...',
+                'autocomplete' => true, // Option Symfony UX
+            ])
+            ->add('paysDepart', CountryType::class, [
+                'placeholder' => 'Rechercher un pays...',
+                'autocomplete' => true, // Option Symfony UX
+            ])
+            ->getForm();
+
         if ($request->isMethod('POST')) {
             // Traitement à venir lorsque nous brancherons la base de données
             $this->addFlash('success', 'Trajet enregistré avec succès (Simulation) !');
             return $this->redirectToRoute('admin_agency_line_index');
         }
 
-        return $this->render('admin/line/add.html.twig', [
-            'page' => 'line'
+        return $this->render('admin/agency/line/add.html.twig', [
+            'page' => 'line',
+            'form' => $form->createView(),
         ]);
     } //add
 }
