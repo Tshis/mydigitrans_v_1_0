10bcb081df12cc8b9dc4c971170960ae95b0728c
tshis <tshisdaniel@gmail.com>
2026-07-14 14:58:51 +0200

    gestion voyage v1

---
+++ src/Controller/web/admin/agency/TripController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller\web\admin\agency;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class TripController extends AbstractController
+{
+
+    #[Route('/admin/agency/trips', name: 'admin_agency_trip_index')]
+    public function index(Request $request): Response
+    {
+        return $this->render('admin/agency/trip/index.html.twig', [
+            'page' => 'trip',
+        ]);
+    } //index
+
+    #[Route('/admin/agency/trip/add', name: 'admin_agency_trip_add')]
+    public function add(Request $request): Response
+    {
+        return $this->render('admin/agency/trip/add.html.twig', [
+            'page' => 'trip',
+        ]);
+    } //add
+
+    #[Route('/admin/agency/trip/show/{code}', name: 'admin_agency_trip_show')]
+    public function show(Request $request): Response
+    {
+        return $this->render('admin/agency/trip/show.html.twig', [
+            'page' => 'trip',
+        ]);
+    } //show
+
+}
