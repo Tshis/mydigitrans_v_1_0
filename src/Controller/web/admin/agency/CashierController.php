32f0902c76abcd619150e067436990647ce49692
tshis <tshisdaniel@gmail.com>
2026-08-03 18:53:48 +0200

    interfaces de la gestion de la caisse

--- src/Controller/web/admin/agency/CashierController.php
+++ src/Controller/web/admin/agency/CashierController.php
@@ -11,10 +11,69 @@ class CashierController extends AbstractController
 {
 
     #[Route('/admin/agency/cash-register', name: 'admin_agency_cashier_index')]
-    public function index(Request $request): Response
+    #[Route('/admin/agency/cash-register', name: 'admin_agency_cashier_dashboard')]
+    public function dashboard(Request $request): Response
     {
-        return $this->render('admin/agency/cashier/index.html.twig', [
+        return $this->render('admin/agency/cashier/dashboard.html.twig', [
             'page' => 'cashier',
         ]);
-    } //index
+    } //dashboard
+
+    #[Route('/admin/agency/cash-register/payment', name: 'admin_agency_cashier_payment')]
+    public function payment(Request $request): Response
+    {
+        return $this->render('admin/agency/cashier/payment.html.twig', [
+            'page' => 'cashier',
+        ]);
+    } //payment
+
+    #[Route('/admin/agency/cash-register/expense', name: 'admin_agency_cashier_expense')]
+    public function expense(Request $request): Response
+    {
+        return $this->render('admin/agency/cashier/expense.html.twig', [
+            'page' => 'cashier',
+        ]);
+    } //expense
+
+    #[Route('/admin/agency/cash-register/payment/code-de-paiement', name: 'admin_agency_cashier_show_payment')]
+    public function show_payment(Request $request): Response
+    {
+        return $this->render('admin/agency/cashier/show_payment.html.twig', [
+            'page' => 'cashier',
+        ]);
+    } //show_payment
+
+    #[Route('/admin/agency/cash-register/expense/code-de-paiement', name: 'admin_agency_cashier_show_expense')]
+    public function show_expense(Request $request): Response
+    {
+        return $this->render('admin/agency/cashier/show_expense.html.twig', [
+            'page' => 'cashier',
+        ]);
+    } //show_expense
+
+    #[Route('/admin/agency/cash-register/dashboard/general', name: 'admin_agency_cashier_dashboard_general')]
+    public function general(Request $request): Response
+    {
+        return $this->render('admin/agency/cashier/general.html.twig', [
+            'page' => 'cashier',
+        ]);
+    } //general
+
+    #[Route('/admin/agency/cash-register/dashboard/branch', name: 'admin_agency_cashier_dashboard_branch')]
+    public function branch(Request $request): Response
+    {
+        return $this->render('admin/agency/cashier/branch.html.twig', [
+            'page' => 'cashier',
+        ]);
+    } //branch
+
+    #[Route('/admin/agency/cash-register/session/closing', name: 'admin_agency_cashier_session_closing')]
+    public function session_closing(Request $request): Response
+    {
+        return $this->render('admin/agency/cashier/session_close.html.twig', [
+            'page' => 'cashier',
+        ]);
+    } //session_closing
+
+
 }
