3dfa71030de630271e2958bbe4195b8def18f8bd
tshis <tshisdaniel@gmail.com>
2026-09-14 17:59:38 +0200

    fleet report design done

--- src/Controller/web/admin/agency/Report/FleetController.php
+++ src/Controller/web/admin/agency/Report/FleetController.php
@@ -10,18 +10,27 @@ final class FleetController extends AbstractController
 {
 
     #[Route('/admin/agency/report/fleet/home', name: 'admin_agency_report_fleet_index')]
-    public function fleetIndex(): Response
+    public function index(): Response
     {
         $vehiclesMatrix = [
             ['brand' => 'Toyota', 'model' => 'Coaster', 'plateNumber' => 'A-5678-DE', 'grossRevenue' => 4850000, 'maintenanceCost' => 450000, 'currency' => 'CDF']
         ];
-        return $this->render('admin/agency/report/fleet/index.html.twig', ['vehicles_matrix' => $vehiclesMatrix]);
-    }
+        return $this->render('admin/agency/report/fleet/index.html.twig', [
+            'page' => 'report',
+            'date_from' => '2026 - 08 - 31',
+            'date_to' => '2026 - 09 - 14',
+            'vehicles_matrix' => $vehiclesMatrix
+        ]);
+    } //index
 
     #[Route('/admin/agency/reports/fleet/{plate}/show', name: 'admin_agency_report_fleet_show')]
-    public function fleetShow(string $plate): Response
+    public function show(string $plate): Response
     {
         $stats = ['occupancyRate' => 78.4, 'totalTrips' => 24, 'kmTraveled' => 8400];
-        return $this->render('admin/agency/report/fleet/show.html.twig', ['plate_number' => $plate, 'stats' => $stats]);
-    }
+        return $this->render('admin/agency/report/fleet/show.html.twig', [
+            'page' => 'report',
+            'plate_number' => $plate,
+            'stats' => $stats
+        ]);
+    } //show
 }
