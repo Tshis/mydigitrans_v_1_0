eb757727b1fcda764308f1472c1d5dde97d3ac8f
tshis <tshisdaniel@gmail.com>
2026-09-15 15:48:11 +0200

    paramettres design done

--- src/Controller/web/admin/security/PasswordController.php
+++ src/Controller/web/admin/security/PasswordController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\web\admin\security;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,6 +16,37 @@ final class PasswordController extends AbstractController
         return $this->render('admin/password/reset.html.twig', [
             'page' => 'agent',
         ]);
-    } //edit
+    } //reset
+
+    #[Route('/admin/password/change', name: 'admin_password_change')]
+    public function change(Request $request): Response
+    {
+
+        if ($request->isMethod('POST')) {
+            $currentPassword = $request->request->get('current_password');
+            $newPassword = $request->request->get('new_password');
+            $confirmPassword = $request->request->get('confirm_password');
+
+            // Validation de conformité des jetons de saisie
+            if ($newPassword !== $confirmPassword) {
+                $this->addFlash('danger', 'La confirmation du nouveau mot de passe ne correspond pas.');
+                return $this->redirectToRoute('admin_agency_account_password');
+            }
+
+            // Ici, ton UserPasswordHasherInterface de Symfony encodera et hydratera l'entité User [MCD 3]
+
+            $this->addFlash('success', 'Votre mot de passe a été modifié avec succès.');
+            return $this->redirectToRoute('admin_agency_account_password');
+        }
+
+
+
+
+        return $this->render('admin/password/change.html.twig', [
+            'page' => 'setting',
+        ]);
+    } //change
+
+
 
 }
